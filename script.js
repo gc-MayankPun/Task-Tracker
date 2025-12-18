7592e315d7db399def25a1e91ffbf06dd1798679
gc_mayank <punmayank35@gmail.com>
2025-12-19 01:52:58 +0530

    Added theme switcher feature

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
   This is just a sample project for my Git crash course. It doesn't do anything.
 */
 
-console.log('Task Tracker Running...');
+console.log("Task Tracker Running...");
+
+document.querySelector(".switcher").addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+  this.classList.toggle("switch-dark");
+});
